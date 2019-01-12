bc1ba20e1e1fd03f03736791e9e0856bb34f227d
Rory Davidson <rorydavidson18@gmail.com>
2019-01-11 16:03:37 -0800

    I added all the spreads of the games to the excel file and then added a function that calculates every teams ATS record

--- nfl_money_lines/nfl_money_lines_calc.py
+++ nfl_money_lines/nfl_money_lines_calc.py
@@ -9,17 +9,38 @@ number_rows = sheet.nrows
 number_columns = sheet.ncols
 games_list = []
 teams_dict = {}
+spread_teams_dict = {}
 
 
-def calculate_payout(odds, bet):
-    if odds==100 or odds==-100:
-        return bet
-    elif odds<0:
-        payout_per_dollar = 100.00/(odds*(-1.00))
+def calculate_moneyline_payout(moneyline, bet):
+    if moneyline==100 or moneyline==-100:
+        return 0
+    elif moneyline<0:
+        payout_per_dollar = 100.00/(moneyline*(-1.00))
         return payout_per_dollar*bet
-    elif odds>0:
-        payout_per_dollar = (odds*1.00)/100.00
+    elif moneyline>0:
+        payout_per_dollar = (moneyline*1.00)/100.00
         return payout_per_dollar*bet
+
+def spread_result(team1, team1_score, team2, team2_score, team1_spread, team2_spread):
+    if team1_spread<0 and team1_score-team2_score>team1_spread:
+        try:
+            spread_teams_dict[team1][1] += 1
+        except KeyError:
+            spread_teams_dict[team1] = [0, 1]
+        try:
+            spread_teams_dict[team2][0] += 1
+        except KeyError:
+            spread_teams_dict[team2] = [1, 0]
+    else:
+        try:
+            spread_teams_dict[team2][1] += 1
+        except KeyError:
+            spread_teams_dict[team2] = [0, 1]
+        try:
+            spread_teams_dict[team1][0] += 1
+        except KeyError:
+            spread_teams_dict[team1] = [1, 0]
 
 
 if __name__ == "__main__":
@@ -30,13 +51,14 @@ if __name__ == "__main__":
             game.append(value)
         games_list.append(game)
     for game in games_list:
+        spread_result(game[0], game[4], game[2], game[5], game[6], game[7])
         if game[4]==game[5]:
             pass
         elif game[4]>game[5]:
             try:
-                teams_dict[game[0]] = teams_dict[game[0]] + calculate_payout(game[1], 100)
+                teams_dict[game[0]] = teams_dict[game[0]] + calculate_moneyline_payout(game[1], 100)
             except KeyError:
-                teams_dict[game[0]] = calculate_payout(game[1], 100)
+                teams_dict[game[0]] = calculate_moneyline_payout(game[1], 100)
 
             try:
                 teams_dict[game[2]] = teams_dict[game[2]] - 100
@@ -44,9 +66,9 @@ if __name__ == "__main__":
                 teams_dict[game[2]] = -100
         else:
             try:
-                teams_dict[game[2]] = teams_dict[game[2]] + calculate_payout(game[3], 100)
+                teams_dict[game[2]] = teams_dict[game[2]] + calculate_moneyline_payout(game[3], 100)
             except KeyError:
-                teams_dict[game[2]] = calculate_payout(game[3], 100)
+                teams_dict[game[2]] = calculate_moneyline_payout(game[3], 100)
             try:
                 teams_dict[game[0]] = teams_dict[game[0]] - 100
             except KeyError:
@@ -55,6 +77,7 @@ if __name__ == "__main__":
     for team in teams_dict:
         teams_dict[team] = round(teams_dict[team], 2)
         print str(team) + ': ' + str(teams_dict[team])
+        print str(team) + ': ' + str(spread_teams_dict[team][0]) + '-' + str(spread_teams_dict[team][1])
     # for foo in teams_dict:
     #     teams_dict[foo] = round(teams_dict[foo], 2)
     #     print str(foo) + ': ' + str(teams_dict[foo])
